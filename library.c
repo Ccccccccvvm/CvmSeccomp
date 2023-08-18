2e1961feb8d0e0527b6af9242a43d1a63caf738f
Ccccccccvm <Gh0717888@gmail.com>
2023-08-18 14:32:33 +0800

    init

---
+++ library.c
@@ -0,0 +1,7 @@
+#include "library.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef CVMSECCOMP_LIBRARY_H
+#define CVMSECCOMP_LIBRARY_H
+
+void hello(void);
+
+#endif //CVMSECCOMP_LIBRARY_H
